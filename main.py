cbc638320883f76371104e628de4110c39fcacdf
Sankha Subhra Mondal <sankhasubhramondal18@gmail.com>
2021-02-16 18:48:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify
 import requests
 import bs4
 from bs4 import BeautifulSoup as bs
 import lxml
+from flask_cors import CORS
 
 
-app= Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
 app.config['JSON_SORT_KEYS'] = False
 
-
-
-def wordmeaning(url,headers):
+def wordMeaning(url,headers):
     response = requests.get(url, headers=headers).text
     soup = bs(response, 'lxml')
     data = soup.find_all('div', class_="definition-wrapper")
@@ -25,11 +25,12 @@ def wordmeaning(url,headers):
             pass
     return meaning
 
-def wordsynonyms(url1,headers):
-    response1 = requests.get(url1, headers=headers).text
-    soup1 = bs(response1, 'lxml')
+
+def wordSynonyms(url,headers):
+    response = requests.get(url, headers=headers).text
+    soup = bs(response, 'lxml')
+    data = soup.find_all('div', class_='synonym-link-wrapper')
     synonyms = []
-    data = soup1.find_all('div', class_='synonym-link-wrapper')
     c = 0
     for i in data:
         if (c < 5):
@@ -37,12 +38,14 @@ def wordsynonyms(url1,headers):
             c += 1
         else:
             pass
+
     return synonyms
 
-def wordsentences(url2,headers):
-    response2 = requests.get(url2, headers=headers).text
-    soup2 = bs(response2, 'lxml')
-    data = soup2.find_all('div', class_='sentence component')
+
+def WordSentences(url,headers):
+    response = requests.get(url, headers=headers).text
+    soup = bs(response, 'lxml')
+    data = soup.find_all('div', class_='sentence component')
     sentences = []
     c = 0
     for i in data:
@@ -51,33 +54,30 @@ def wordsentences(url2,headers):
             c += 1
         else:
             pass
-
     return sentences
 
 
 
-@app.route('/<string:word>')
+@app.route('/api/<string:word>')
 def data(word):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-    }
-
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     url = 'https://www.yourdictionary.com/{}'.format(word)
     url1 = 'https://thesaurus.yourdictionary.com/{}'.format(word)
     url2 = 'https://sentence.yourdictionary.com/{}'.format(word)
 
-    meaning = wordmeaning(url,headers)
-    synonyms = wordsynonyms(url1,headers)
-    sentences = wordsentences(url2,headers)
+    meanings = wordMeaning(url,headers)
+    synonyms = wordSynonyms(url1,headers)
+    sentences = WordSentences(url2,headers)
 
     result = {
         'word': word,
-        'meaning':meaning,
+        'meaning':meanings,
         'synonyms':synonyms,
-        'sentences':sentences,
+        'sentences':sentences
     }
-
     return jsonify(result)
+
+
 
 
 
